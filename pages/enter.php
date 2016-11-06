b9fdde36ff676754607c34761df2cec1b50560ec
vladimir1727 <dvn125@gmail.com>
2016-11-06 07:49:34 +0200

    home1

--- pages/enter.php
+++ pages/enter.php
@@ -1,3 +1,11 @@
+<?php 
+if (isset($_POST['adduser'])) {
+	if(register($_POST['login'],$_POST['pass1'],$_POST['email'])){
+		echo '<h3 style="color:green;">Добавлен</h3>';
+	}
+}
+else{
+?>
 <form action="" class="input-group input-group-sm">
 
 		<input type="text" name="login" class="form-control" placeholder="логин">
@@ -5,3 +13,6 @@
 		<input type="submit" id="press" value="войти" class="btn btn-default btn-sm">
 
 </form>
+<?php
+}
+?>
